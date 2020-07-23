484f05c047587c958f4c6cf00ec371590a2ddc74
vijayban007 <68693796+vijayban007@users.noreply.github.com>
2020-07-23 19:03:59 +0530

    Add files via upload

---
+++ Vijay Batch 6.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print (" Hello ")
+
+
+# In[6]:
+
+
+print ( " Hello Vijay ")
+
+
+# In[7]:
+
+
+print ( """ I love you """)
+
+
+# In[9]:
+
+
+print ( " Vijay's Python")
+
+
+# In[10]:
+
+
+print ( " Hello \t python")
+
+
+# In[11]:
+
+
+print ( 'Vijay\'s Python')
+
+
+# In[15]:
+
+
+print ("Vijay\n Python")
+
+
+# In[21]:
+
+
+print ("Vijay\ t Python")
+
+
+# In[3]:
+
+
+name = "Vijay"
+marks = 89.99
+age = 35
+print ("The name of person is",name,"marks is", marks,"age is", age)
+
+
+# In[4]:
+
+
+name = " Vijay"
+marks = 89.99
+age = 35
+print ( " The name of person is %s marks is %f age is % d"%(name, marks, age))
+
+
+# In[5]:
+
+
+name = " Vijay"
+marks = 89.899
+age = 35
+print ( " The name of person is %1s marks is %.2f age is %d"%( name, marks,age))
+
+
+# In[9]:
+
+
+print (f" The name of person is {name} the marks is {marks} age is {age}")
+
+
+# In[11]:
+
+
+10**10
+
+
+# In[12]:
+
+
+150**10
+
+
+# In[13]:
+
+
+250+350
+
+
+# In[14]:
+
+
+150-250
+
+
+# In[15]:
+
+
+50/2
+
+
+# In[16]:
+
+
+100/3
+
+
+# In[17]:
+
+
+100%3
+
+
+# In[18]:
+
+
+100//3
+
+
+# In[19]:
+
+
+a=100
+b=200
+a==b
+
+
+# In[20]:
+
+
+c=100
+a==c
+
+
+# In[21]:
+
+
+c<a
+
+
+# In[22]:
+
+
+c>a
+
+
+# In[23]:
+
+
+c<=a
+
+
+# In[24]:
+
+
+c>=a
+
+
+# In[25]:
+
+
+c>b
+
+
+# In[26]:
+
+
+c<b
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
